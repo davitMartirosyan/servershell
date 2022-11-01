eac6faf16ae246accd732b22897f347a4be61244
Khachatur <avetisyan.khachatur@student.rau.am>
2022-11-01 16:57:56 +0400

    merge commit

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 
 all:
-	$(CC) serversrc/*.c -o server
+	$(CC) serversrc/init.c serversrc/server.c -o server
 	$(CC) clientsrc/*.c -lreadline -o client
 
 #send_read_msg.o:
--- serversrc/server.c
+++ serversrc/server.c
@@ -1,5 +1,8 @@
 # include "../include/servershell.h"
+#include "../utils/send_read_msg.c"
+
 # define PORT 8080
+#define EXIT_MSG "bye"
 
 int main(int ac, char **av, char **envp)
 {
@@ -34,7 +37,18 @@ int main(int ac, char **av, char **envp)
 
          while(1)
          {
+             read_msg(table->socket_client_fd, &table->cmdline, &table->size_cmdline);
 
+             printf("Server: command output size of ~ %hd\n", table->size_cmdline);
+             printf("Server: command output ~ %s\n", table->cmdline);
+
+             if (!strcmp(table->cmdline, EXIT_MSG))
+                 break;
+
+             table->cmd_output = "hello aper!!))";
+             table->size_output = (int16_t) strlen(table->cmd_output);
+
+             send_msg(table->socket_client_fd, table->cmd_output, table->size_output);
          }
 
         // closing the connected socket
